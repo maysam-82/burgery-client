f275140dc7dfd532db23c649b4f7b3a695d2467c
maysam <maysam82.dev@gmail.com>
2020-09-01 21:10:53 +1000

    Project init.

---
+++ src/components/App/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import classes from './App.module.scss';
+function App() {
+    return <div className={classes.appContainer}>Sample Application</div>;
+}
+
+export default App;
