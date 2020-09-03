76e4bb12f52b5e15c0ef58b1471d940af9d095de
maysam <maysam82.dev@gmail.com>
2020-09-04 02:00:05 +1000

    Added Spinner and error handling HOC

--- src/container/BurgerBuilder/BurgerBuilder.tsx
+++ src/container/BurgerBuilder/BurgerBuilder.tsx
@@ -1,11 +1,14 @@
 import React, { Component, Fragment } from 'react';
+import axios from '../../services/api/axios-orders';
 import Burger from '../../components/Burger';
 import { IIngredients } from '../../types/ingredients';
 import BurgerControls from '../../components/Burger/BurgerControls';
 import { ingredientsPrices } from '../../fixtures/ingredients';
 import { getIngredients } from '../../components/utils/burger';
 import Modal from '../../components/Modal';
-import OrderSummary from '../../components/OrderSummary/OrderSummary';
+import OrderSummary from '../../components/OrderSummary';
+import Spinner from '../../components/Spinner';
+import WithErrorHandler from '../../HOC/WithErrorHandler';
 
 import classes from './burgerBuilder.module.scss';
 
@@ -14,6 +17,7 @@ interface IBurgerBuilderState {
     totalPrice: number;
     purchasable: boolean;
     isOrdered: boolean;
+    isLoading: boolean;
 }
 
 class BurgerBuilder extends Component<{}, IBurgerBuilderState> {
@@ -29,6 +33,7 @@ class BurgerBuilder extends Component<{}, IBurgerBuilderState> {
             totalPrice: 4,
             purchasable: false,
             isOrdered: false,
+            isLoading: false,
         };
     }
 
@@ -77,19 +82,58 @@ class BurgerBuilder extends Component<{}, IBurgerBuilderState> {
         this.setState({ isOrdered: false });
     };
 
+    handlePurchaseContinue = () => {
+        const { ingredients, totalPrice } = this.state;
+        this.setState({ isLoading: true });
+        const order = {
+            ingredients,
+            totalPrice,
+            customer: {
+                name: 'test',
+                address: {
+                    street: 'test street',
+                    zipCode: 'xxxxxx',
+                    city: 'test city',
+                },
+                email: 'sample@sample.com',
+            },
+            deliveryMethod: 'fastest',
+            comments: 'without tomato',
+        };
+        axios
+            .post('/orders.json', order)
+            .then((response) =>
+                this.setState({ isLoading: false, isOrdered: false })
+            )
+            .catch((error) =>
+                this.setState({ isLoading: false, isOrdered: false })
+            );
+    };
     render() {
-        const { ingredients, totalPrice, purchasable, isOrdered } = this.state;
+        const {
+            ingredients,
+            totalPrice,
+            purchasable,
+            isOrdered,
+            isLoading,
+        } = this.state;
+        const renderOrderSummary = isLoading ? (
+            <Spinner />
+        ) : (
+            <OrderSummary
+                ingredients={ingredients}
+                handleCancel={this.handleCancelPurchase}
+                handleContinue={this.handlePurchaseContinue}
+                price={totalPrice}
+            />
+        );
         return (
             <Fragment>
                 <Modal
                     isShown={isOrdered}
                     handleModalClose={this.handleCancelPurchase}
                 >
-                    <OrderSummary
-                        ingredients={ingredients}
-                        handleCancel={this.handleCancelPurchase}
-                        price={totalPrice}
-                    />
+                    {renderOrderSummary}
                 </Modal>
                 <div className={classes.burgerBuilderContainer}>
                     <Burger ingredients={ingredients} />
@@ -105,4 +149,4 @@ class BurgerBuilder extends Component<{}, IBurgerBuilderState> {
     }
 }
 
-export default BurgerBuilder;
+export default WithErrorHandler(BurgerBuilder, axios);
