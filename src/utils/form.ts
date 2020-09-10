976b94a01cf1badb794ee5ee96c649d17fee4709
maysam <maysam82.dev@gmail.com>
2020-09-11 00:54:18 +1000

    removed extra escape char

--- src/utils/form.ts
+++ src/utils/form.ts
@@ -16,7 +16,7 @@ export const handleControlUpdate = <T extends DataType>(
 
 // validation for email address
 export const validateEmail = (email: string): boolean => {
-    const regexValidationTerm = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const regexValidationTerm = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return regexValidationTerm.test(email);
 };
 
