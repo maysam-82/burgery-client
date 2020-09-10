90509a0ebc373721bba01a9f5e1ed6edef454624
maysam <maysam82.dev@gmail.com>
2020-09-10 15:26:52 +1000

    Created Confirmation page with relevant route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import BurgerBuilder from '../../container/BurgerBuilder';
 import Checkout from '../../container/Checkout';
 import { Route, Switch } from 'react-router-dom';
 import Orders from '../../container/Orders';
+import Confirmation from '../Confirmation/Confirmation';
 
 import classes from './App.module.scss';
 
@@ -12,6 +13,7 @@ function App() {
         <div className={classes.appContainer}>
             <Layout>
                 <Switch>
+                    <Route path="/confirmation" component={Confirmation} />
                     <Route path="/checkout" component={Checkout} />
                     <Route path="/orders" component={Orders} />
                     <Route path="/" exact component={BurgerBuilder} />
---
+++ src/components/Confirmation/Confirmation.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Button from '../Button/Button';
+import { Redirect } from 'react-router-dom';
+import history from '../../history';
+
+import classes from './confirmation.module.scss';
+
+interface IConfirmationProps {
+    orderNumber: string;
+}
+
+function Confirmation({ orderNumber }: IConfirmationProps) {
+    return orderNumber ? (
+        <div className={classes.confirmationContainer}>
+            <h4 className={classes.confirmationTitle}>
+                Your order is submited.
+            </h4>
+            <div className={classes.confrimationNumber}>
+                <p className={classes.confrimationLabel}>Your order Number</p>
+                {orderNumber}
+            </div>
+            <div className={classes.buttonContainer}>
+                <Button type="success" handleClick={() => history.replace('/')}>
+                    Home
+                </Button>
+            </div>
+        </div>
+    ) : (
+        <Redirect to="/" />
+    );
+}
+
+export default Confirmation;
