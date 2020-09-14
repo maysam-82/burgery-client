ef39dfb39693c346404304f7dc05c50fb4ff97f4
maysam <maysam82.dev@gmail.com>
2020-09-14 17:22:22 +1000

    Use React.lazy to optimize app

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { connect } from 'react-redux';
 import Layout from '../Layout/Layout';
 import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
-import Checkout from '../Checkout/Checkout';
 import { Route, Switch } from 'react-router-dom';
-import Orders from '../Orders/Orders';
-import Confirmation from '../../components/Confirmation/Confirmation';
-import Authentication from '../Authentication/Authentication';
-import Logout from '../Logout/Logout';
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
-import NotFound from '../../components/NotFound/NotFound';
 import { authCheckState } from '../../redux/actions/auth';
 
 import classes from './App.module.scss';
@@ -17,6 +11,13 @@ import classes from './App.module.scss';
 interface IAppProps {
     authCheckState: Function;
 }
+
+const asyncCheckout = lazy(() => import('../Checkout'));
+const asyncOrders = lazy(() => import('../Orders'));
+const asyncAuthentication = lazy(() => import('../Authentication'));
+const asyncLogout = lazy(() => import('../Logout'));
+const asyncNotFound = lazy(() => import('../../components/NotFound'));
+const asyncConfirmation = lazy(() => import('../../components/Confirmation'));
 
 class App extends Component<IAppProps> {
     componentDidMount() {
@@ -27,16 +28,33 @@ class App extends Component<IAppProps> {
         return (
             <div className={classes.appContainer}>
                 <Layout>
-                    <Switch>
-                        <Route path="/" exact component={BurgerBuilder} />
-                        <Route path="/login" component={Authentication} />
-                        <Route path="/register" component={Authentication} />
-                        <Route path="/checkout" component={Checkout} />
-                        <Route path="/confirmation" component={Confirmation} />
-                        <PrivateRoute path="/orders" component={Orders} />
-                        <PrivateRoute path="/logout" component={Logout} />
-                        <Route component={NotFound} />
-                    </Switch>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Switch>
+                            <Route path="/" exact component={BurgerBuilder} />
+                            <Route
+                                path="/login"
+                                component={asyncAuthentication}
+                            />
+                            <Route
+                                path="/register"
+                                component={asyncAuthentication}
+                            />
+                            <Route path="/checkout" component={asyncCheckout} />
+                            <Route
+                                path="/confirmation"
+                                component={asyncConfirmation}
+                            />
+                            <PrivateRoute
+                                path="/orders"
+                                component={asyncOrders}
+                            />
+                            <PrivateRoute
+                                path="/logout"
+                                component={asyncLogout}
+                            />
+                            <Route component={asyncNotFound} />
+                        </Switch>
+                    </Suspense>
                 </Layout>
             </div>
         );
