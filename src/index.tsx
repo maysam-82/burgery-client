4772787bb7653f6f307ea5073535e1e80abbad08
maysam <maysam82.dev@gmail.com>
2020-09-14 16:44:13 +1000

    set environment variable to show redux in dev mode

--- src/index.tsx
+++ src/index.tsx
@@ -17,11 +17,15 @@ declare global {
     }
 }
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers =
+    process.env.NODE_ENV === 'development'
+        ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        : null || compose;
 
 const globalStore = createStore(
     reducers,
-    composeEnhancers(applyMiddleware(thunk))
+    // using ! to ensure composeEnhancer is not null.
+    composeEnhancers!(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
